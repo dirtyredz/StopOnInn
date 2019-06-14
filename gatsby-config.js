c4ddb6ced303d884e786b57a29017d8c12b896dc
dirtyredz <dirtyredz@live.com>
2019-06-14 18:33:42 -0500

    Fixed SPA

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,7 +36,7 @@ module.exports = {
         short_name: `SOI`,
         start_url: `/`,
         background_color: Colors.White,
-        theme_color: Colors.Red,
+        theme_color: Colors.DarkGreen,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
