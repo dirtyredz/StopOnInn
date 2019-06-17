a5bbbbf15760bbc37d908f25e115b9fe609c0f25
dirtyredz <dirtyredz@live.com>
2019-06-17 18:50:27 -0500

    Add service worker update message test

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -51,3 +51,14 @@ export const onClientEntry = () => {
     }
   });
 };
+
+export const onServiceWorkerUpdateReady = () => {
+  const answer = window.confirm(
+    `This application has been updated. ` +
+      `Reload to display the latest version?`
+  )
+
+  if (answer === true) {
+    window.location.reload()
+  }
+}
